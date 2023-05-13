5ea8d704858a4ceaea1175a1955a431cc57e6dd7
Fitsum19 <117902393+Fitsum19@users.noreply.github.com>
2023-05-13 12:12:15 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines unittests for models/user.py.
-Unittest classes:
-    TestUser_instantiation
-    TestUser_save
-    TestUser_to_dict
+Uni ttest classes:
+    Test User_instantiation
+    Test User_save
+    Test User_to_dict
 """
 import os
 import models
