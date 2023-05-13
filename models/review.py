2b9c59cc567c529086091a520b462790f98dfb31
Fitsum19 <117902393+Fitsum19@users.noreply.github.com>
2023-05-13 12:02:10 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Class Review """
+""" ClassReview """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review class that inherits BaseModel """
+    """ Review class that inherits Base Model """
     place_id = ""
     user_id = ""
     text = ""
