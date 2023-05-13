cac05f9bfdb067f2407ea25fe6b79ec141e6c3cc
Fitsum19 <117902393+Fitsum19@users.noreply.github.com>
2023-05-13 12:13:18 +0300

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines unittests for models/state.py.
-Unittest classes:
-    TestState_instantiation
-    TestState_save
-    TestState_to_dict
+Unit test classes:
+    Test State_instantiation
+    Test State_save
+    Test State_to_dict
 """
 import os
 import models
